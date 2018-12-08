e4dc874d03fb09fec200cf2fcccd42110468a119
pauloupgrad <pauloupgrad@gmail.com>
2018-12-08 17:06:41 -0200

    Colocado as moedas de forma aleatória no labirinto

--- js/stage1.js
+++ js/stage1.js
@@ -16,8 +16,13 @@ var stage1State = {
             
         ];
 
+        //CRIAÇÃO DO GRUPO DE BLOCOS
         this.blocks = game.add.group();
         this.blocks.enableBody = true;
+        
+        //array das posiveis posições das coins no labirinto
+        this.coinPositions = [];
+
 
         for(var row in this.maze){
             for(var col in this.maze[row]){
@@ -33,11 +38,27 @@ var stage1State = {
                     this.player = game.add.sprite(x + 25, y + 25, 'player');
                     this.player.anchor.set(.5);
                     game.physics.arcade.enable(this.player);
-
-
+                    this.player.animations.add('goDown',[0,1,2,3,4,5,6,7],12,true);
+                    this.player.animations.add('goUp',[8,9,10,11,12,13,14,15],12,true);
+                    this.player.animations.add('goLeft',[16,17,18,19,20,21,22,23],12,true);
+                    this.player.animations.add('goRight',[24,25,26,27,28,29,30,31],12,true);
+                } else if(tile === 3){
+                    var position = {
+                        x: x + 25,
+                        y: y + 25
+                    };
+                    //Colocando obj position no array coinPositions
+                    this.coinPositions.push(position);
                 }
             }//Fim for col
         }//Fim for row
+
+        //CRIAÇÂO DAS MOEDAS
+        this.coin = {};
+        this.coin.position = this.newPosition();
+        this.coin = game.add.sprite(this.coin.position.x, this.coin.position.y,'coin');
+        this.coin.anchor.set(.5);
+        this.coin.animations.add('spin',[0,1,2,3,4,5,6,7,8,9],10,true).play();
 
         //Controles
         this.controls = game.input.keyboard.createCursorKeys();
@@ -53,19 +74,51 @@ var stage1State = {
     movePlayer: function(){
         this.player.body.velocity.x = 0;
         this.player.body.velocity.y = 0;
-
+        //Esquerda ou Direita
         if(this.controls.left.isDown && !this.controls.right.isDown){
             this.player.body.velocity.x = -100;
-        }
-        if(this.controls.right.isDown && !this.controls.left.isDown){
+            this.player.direction = "left";
+        } else if(this.controls.right.isDown && !this.controls.left.isDown){
             this.player.body.velocity.x = 100;
-        }
-        if(this.controls.up.isDown && !this.controls.down.isDown){
-            this.player.body.velocity.y = -100;
-        }
-        if(this.controls.down.isDown && !this.controls.up.isDown){
-            this.player.body.velocity.y = 100;
+            this.player.direction = "right";
         }
 
-    }
+        //Para Cima ou para Baixo
+        if(this.controls.up.isDown && !this.controls.down.isDown){
+            this.player.body.velocity.y = -100;
+            this.player.direction = "up";
+        } else if(this.controls.down.isDown && !this.controls.up.isDown){
+            this.player.body.velocity.y = 100;
+            this.player.direction = "down";
+        }
+
+        //Verificar direção que o player se move
+        switch(this.player.direction){
+            case "left":
+                this.player.animations.play('goLeft'); break;
+            case "right":
+                this.player.animations.play('goRight'); break; 
+            case "up":
+                this.player.animations.play('goUp'); break;
+            case "down":
+                this.player.animations.play('goDown'); break;       
+        }//Fim switch
+
+        //Para animação quando soltar tecla
+        if(this.player.body.velocity.x === 0 && this.player.body.velocity.y === 0){
+            this.player.animations.stop();
+        }
+
+    },//Fim movePlayer
+
+    //Sorteia posições das moedas
+    newPosition: function(){
+        var pos = this.coinPositions[Math.floor(Math.random() * this.coinPositions.length)];
+
+        while(this.coin.position === pos){
+            pos = this.coinPositions[Math.floor(Math.random() * this.coinPositions.length)];
+        }
+
+        return pos;
+    }//Fim newPosition
 };
